fd6f19b2a229b09090c208df6f79697ad185cb99
nissatester-cpu <nissa.tester@gmail.com>
2025-11-14 17:57:35 +0530

    Add files via upload
    Datatypes Home assignment - Week1 day1

---
+++ Chrome.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		//version of type float with a value of 91.0f 
+		float version = 91.0f;
+		
+		//developer of type String with a value of "Google" 
+		String developer = "Google";
+		
+		//isBeta of type boolean with a value of false 
+		boolean isBeta = false;
+		
+		//releaseYear of type int with a value of 2008 
+		int releaseYear = 2008;
+		
+		//shortcutKey of type char with a value of 'C'
+		char shortcutKey = 'C';
+		
+		System.out.println("Version is "+version+", "+"Developer is "+developer+", "+"isBeta is "+isBeta+", "+"Releaseyear is "+releaseYear+", "+"Shortcutkey is "+shortcutKey);
+		
+	}
+
+}
