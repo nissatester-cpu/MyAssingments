18c51e41fef0f5f6712b6d9872e1deafa810c12b
nissatester-cpu <nissa.tester@gmail.com>
2026-04-25 20:49:24 +0530

    Add files via upload
    Day1 assignment

---
+++ Chrome.java
@@ -0,0 +1,18 @@
+package org.testleaf.week1.day1;
+
+public class Chrome {
+public static void main(String[] args) {
+    
+    float version = 91.0f;
+    String developer = "Google";
+    boolean isBeta = false;
+    int releaseYear = 2008;
+    char shortcutKey = 'C';
+    System.out.println("Version is "+version);
+    System.out.println("Developer is "+developer);
+    System.out.println("Is Beta is "+isBeta);
+    System.out.println("Release year is "+releaseYear);
+    System.out.println("Short cut key is "+shortcutKey);
+    
+}
+}
